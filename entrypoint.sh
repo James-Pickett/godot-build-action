2466333d09832fc33fcc84d003e70180e19bbd94
James Pickett <James-Pickett@users.noreply.github.com>
2019-12-15 21:02:17 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export path_to_editor_settings=/root/.config/godot/editor_settings-3.tres
 /edit-tres.sh $path_to_editor_settings export/android/debug_keystore_pass ${INPUT_ANDROID_DEBUG_PASSWORD}
 
 # create debug keystore
-RUN mkdir -p usr/android-keys
+mkdir -p usr/android-keys
 cd /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin
 keytool -keyalg RSA -genkeypair -alias ${INPUT_ANDROID_DEBUG_USERNAME} -keypass ${INPUT_ANDROID_DEBUG_PASSWORD} -keystore debug.keystore -storepass ${ANDROID_DEBUG_PASSWORD} -dname "CN=Android Debug,O=Android,C=US" -validity 9999
 mv debug.keystore /usr/android-keys/debug.keystore
