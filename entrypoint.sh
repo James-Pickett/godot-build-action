9a1a0e473467c6981902a26e0169ebd37237c242
James Pickett <James-Pickett@users.noreply.github.com>
2019-12-15 20:16:13 -0800

    updated action yaml to be more descriptive changed project var to build-file-name
    updated action yaml to be more descriptive changed project var to build-file-name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,26 @@
 set -e
 
 # Export for HTML5
-echo "Building ${PROJECT} for Android"
+echo "Building ${build-file-name} for Android"
 mkdir -p ./build/android/
-godot --export "Android" ./build/android/${PROJECT}.apk
+godot --export "Android" ./build/android/${build-file-name}.apk
 
 # Export for Linux
-echo "Building ${PROJECT} for Linux"
+echo "Building ${build-file-name} for Linux"
 mkdir -p ./build/linux/
-godot --export Linux/X11 ./build/linux/${PROJECT}
+godot --export Linux/X11 ./build/linux/${build-file-name}
 
 # Export for Windows
-echo "Building ${PROJECT} for Windows"
+echo "Building ${build-file-name} for Windows"
 mkdir -p ./build/windows/
-godot --export "Windows Desktop" ./build/windows/${PROJECT}.exe
+godot --export "Windows Desktop" ./build/windows/${build-file-name}.exe
 
 # Export for OSX
-echo "Building ${PROJECT} for OSX"
+echo "Building ${build-file-name} for OSX"
 mkdir -p ./build/mac/
-godot --export "Mac OSX" ./build/mac/${PROJECT}
+godot --export "Mac OSX" ./build/mac/${build-file-name}
 
 # Export for HTML5
-echo "Building ${PROJECT} for HTML5"
+echo "Building ${build-file-name} for HTML5"
 mkdir -p ./build/html5/
 godot --export "HTML5" ./build/html5/index.html
