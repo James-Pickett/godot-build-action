2af41fe92c2d486f5e7184d270aceefbf8e5d585
James Pickett <James-Pickett@users.noreply.github.com>
2019-12-15 21:09:26 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,32 +9,30 @@ export path_to_editor_settings=/root/.config/godot/editor_settings-3.tres
 
 # create debug keystore
 mkdir -p usr/android-keys
-cd /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin
-keytool -keyalg RSA -genkeypair -alias ${INPUT_ANDROID_DEBUG_USERNAME} -keypass ${INPUT_ANDROID_DEBUG_PASSWORD} -keystore debug.keystore -storepass ${ANDROID_DEBUG_PASSWORD} -dname "CN=Android Debug,O=Android,C=US" -validity 9999
-mv debug.keystore /usr/android-keys/debug.keystore
-cd /
+/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/keytool -genkeypair -keyalg RSA -alias "${INPUT_ANDROID_DEBUG_USERNAME}" -keypass "${INPUT_ANDROID_DEBUG_PASSWORD}" -keystore debug.keystore -storepass "${ANDROID_DEBUG_PASSWORD}" -dname "CN=Android Debug,O=Android,C=US" -validity 9999
+mv /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/debug.keystore /usr/android-keys/debug.keystore
 
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for Android"
 mkdir -p ./build/android/
-godot --export "Android" ./build/android/${INPUT_BUILD_FILE_NAME}.apk
+godot --export "Android" ./build/android/${INPUT_BUILD_FILE_NAME}.apk -v
 
 # Export for Linux
 echo "Building ${INPUT_BUILD_FILE_NAME} for Linux"
 mkdir -p ./build/linux/
-godot --export Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME}
+godot --export Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME} -v
 
 # Export for Windows
 echo "Building ${INPUT_BUILD_FILE_NAME} for Windows"
 mkdir -p ./build/windows/
-godot --export "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe
+godot --export "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe -v
 
 # Export for OSX
 echo "Building ${INPUT_BUILD_FILE_NAME} for OSX"
 mkdir -p ./build/mac/
-godot --export "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME}
+godot --export "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME} -v
 
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for HTML5"
 mkdir -p ./build/html5/
-godot --export "HTML5" ./build/html5/index.html
+godot --export "HTML5" ./build/html5/index.html -v
