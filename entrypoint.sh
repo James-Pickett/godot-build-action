47a4c6f40f94dc7431e8d856aefb5b5104f9e988
James Pickett <James-Pickett@users.noreply.github.com>
2019-12-15 21:36:19 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,24 +15,24 @@ mv ./debug.keystore /usr/android-keys/debug.keystore
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for Android"
 mkdir -p ./build/android/
-godot --export "Android" ./build/android/${INPUT_BUILD_FILE_NAME}.apk -v
+godot --export project.godot "Android" ./build/android/${INPUT_BUILD_FILE_NAME}.apk -v
 
 # Export for Linux
 echo "Building ${INPUT_BUILD_FILE_NAME} for Linux"
 mkdir -p ./build/linux/
-godot --export Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME} -v
+godot --export project.godot Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME} -v
 
 # Export for Windows
 echo "Building ${INPUT_BUILD_FILE_NAME} for Windows"
 mkdir -p ./build/windows/
-godot --export "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe -v
+godot --export project.godot "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe -v
 
 # Export for OSX
 echo "Building ${INPUT_BUILD_FILE_NAME} for OSX"
 mkdir -p ./build/mac/
-godot --export "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME} -v
+godot --export project.godot "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME} -v
 
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for HTML5"
 mkdir -p ./build/html5/
-godot --export "HTML5" ./build/html5/index.html -v
+godot --export project.godot "HTML5" ./build/html5/index.html -v
