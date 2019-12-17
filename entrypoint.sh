032f05e961e9c5a7be6cb4523ab99fdff753ec7c
James Pickett <James-Pickett@users.noreply.github.com>
2019-12-16 22:47:02 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,9 +29,6 @@ mkdir -p /usr/android-keys/
 /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/keytool -genkeypair -keyalg RSA -alias "${INPUT_ANDROID_DEBUG_USERNAME}" -keypass "${INPUT_ANDROID_DEBUG_PASSWORD}" -keystore debug.keystore -storepass "${INPUT_ANDROID_DEBUG_PASSWORD}" -dname "CN=Android Debug,O=Android,C=US" -validity 9999
 mv ./debug.keystore /usr/android-keys/debug.keystore
 
-pwd
-ls
-
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for Android"
 mkdir -p ./build/android/
@@ -40,19 +37,19 @@ godot -e -v --export-debug Android ./build/android/${INPUT_BUILD_FILE_NAME}.apk
 # Export for Linux
 echo "Building ${INPUT_BUILD_FILE_NAME} for Linux"
 mkdir -p ./build/linux/
-godot --export Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME} -v
+godot -e -v --export-debug Linux/X11 ./build/linux/${INPUT_BUILD_FILE_NAME}
 
 # Export for Windows
 echo "Building ${INPUT_BUILD_FILE_NAME} for Windows"
 mkdir -p ./build/windows/
-godot --export "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe -v
+godot -e -v --export-debug "Windows Desktop" ./build/windows/${INPUT_BUILD_FILE_NAME}.exe
 
 # Export for OSX
 echo "Building ${INPUT_BUILD_FILE_NAME} for OSX"
 mkdir -p ./build/mac/
-godot --export "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME} -v
+godot -e -v --export-debug "Mac OSX" ./build/mac/${INPUT_BUILD_FILE_NAME}
 
 # Export for HTML5
 echo "Building ${INPUT_BUILD_FILE_NAME} for HTML5"
 mkdir -p ./build/html5/
-godot --export "HTML5" ./build/html5/index.html -v
+godot -e -v --export-debug "HTML5" ./build/html5/index.html
